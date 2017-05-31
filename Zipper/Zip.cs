0c41078c9d2c3e5fe82f8f78aad9d35792425f85
Aidan Timson <atimson123@gmail.com>
2017-05-31 09:22:32 +0100

    Initial commit

---
+++ Zipper/Zip.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Threading;
+
+namespace Zipper
+{
+    public class Zip
+    {
+        public static string ZipFiles(string[] filesToZipArray, string zipPath)
+        {
+            try
+            {
+                using (ZipArchive archive = ZipFile.Open(zipPath, ZipArchiveMode.Create))
+                {
+                    foreach (var file in filesToZipArray)
+                    {
+                        archive.CreateEntryFromFile(file, Path.GetFileName(file));
+                    }
+                }
+                return zipPath;
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+
+        public static string ZipDirectory(string zipDirectory, string zipPath)
+        {
+            try
+            {
+                ZipFile.CreateFromDirectory(zipDirectory, zipPath);
+                Thread.Sleep(2000);
+                return zipPath;
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+
+    }
+}
